0ad78ee103573d13b3538d372144592310cbe098
Oudam Meas <oudam.meas@gmail.com>
2020-08-06 23:54:55 -0400

    FInal Commit

--- src/app.js
+++ src/app.js
@@ -2,20 +2,30 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
-import { NavBar, Footer } from "./components";
+import { NavBar, Footer, Loading, PrivateRoute } from "./components";
 import { Home, Profile, ExternalApi } from "./views";
 
 import "./app.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
+
+  const {isLoading}  = useAuth0();
+
+  if (isLoading) {
+
+    return <Loading />; 
+
+  };
+
   return (
     <div id="app" className="d-flex flex-column h-100">
       <NavBar />
       <Container className="flex-grow-1 mt-5">
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/external-api" component={ExternalApi} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <PrivateRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </Container>
       <Footer />
